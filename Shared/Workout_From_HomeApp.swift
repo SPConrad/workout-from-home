61355a396d679e23afc785a1bfe6ac538ffa9642
Sean Conrad <spconrad@gmail.com>
2021-01-10 22:29:02 -0500

    Initial Commit

---
+++ Shared/Workout_From_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_From_HomeApp.swift
+//  Shared
+//
+//  Created by Sean Conrad on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Workout_From_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
