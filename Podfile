68b22baec3d2376bd293cb47a00c656fc0bb16c4
Sean Conrad <spconrad@gmail.com>
2021-02-16 15:01:01 -0500

    First commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Workout From Home (iOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Workout From Home (iOS)
+
+end
+
+target 'Workout From Home (macOS)' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Workout From Home (macOS)
+
+end
---
+++ Shared/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Workout From Home
+//
+//  Created by Sean Conrad on 1/19/21.
+//
+
+import Foundation
+
+
+public struct Constants {
+    let baseURL = "https://sheets.googleapis.com/v4/spreadsheets/%@/values:batchGet"
+    let gid = "1666571352"
+    let spreadsheetId = "1pgjJRyFc9Tk3FavROACfssT48a7UWLEJ1RITDhWoHcA"
+}
+//https://sheets.googleapis.com/v4/spreadsheets/1pgjJRyFc9Tk3FavROACfssT48a7UWLEJ1RITDhWoHcA/values:batchGet
+//https://sheets.googleapis.com/v4/spreadsheets/1666571352/values:batchGet
